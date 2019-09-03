c0f457ea131c988916fb71e7c7bd28a353c226c2
Gareth Murphy <garethcmurphy@gmail.com>
2019-09-03 13:51:37 +0200

    fix path

--- deployriot.sh
+++ deployriot.sh
@@ -5,11 +5,11 @@ helm del --purge riot
 
 if  [ "$(hostname)" == "scicat01.esss.lu.se" ]; then
     envarray=(ess)
-    INGRESS_NAME="-f ./dacat-gui/lund.yaml"
+    INGRESS_NAME="-f ./riot/lund.yaml"
     BUILD="false"
 elif  [ "$(hostname)" == "k8-lrg-serv-prod.esss.dk" ]; then
     envarray=(dmscprod)
-    INGRESS_NAME="-f ./dacat-gui/dmscprod.yaml"
+    INGRESS_NAME="-f ./riot/dmscprod.yaml"
     BUILD="false"
 fi
 
