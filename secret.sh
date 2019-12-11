84918eed17f58a61ad12f9315d58c17e6cba96db
Henrik Johansson <henrik.johansson712@gmail.com>
2019-12-11 15:43:13 +0100

    Updated secret.sh

--- secret.sh
+++ secret.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
-if [ "$#" -ne 1 ]; then
-  echo "Usage ./secret.sh ENVIRONMENT" >&2
-  exit 1
-fi
-export env=$1
 
-	openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout riot.key -out riot.crt -subj "/CN=scicat01.esss.lu.se" -days 3650
+	openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout riot-dev.key -out riot-dev.crt -subj "/CN=scicat05.esss.lu.se" -days 3650
+	openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout riot-qa.key -out riot-qa.crt -subj "/CN=scicat06.esss.lu.se" -days 3650
+	openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout riot-production.key -out riot-production.crt -subj "/CN=scichat.esss.lu.se" -days 3650
 
 
-	kubectl create secret -n${env} tls riotservice --key riot.key --cert riot.crt
+	kubectl create secret -ndev tls riotservice --key riot-dev.key --cert riot-dev.crt
+	kubectl create secret -nqa tls riotservice --key riot-qa.key --cert riot-qa.crt
+	kubectl create secret -nproduction tls riotservice --key riot-production.key --cert riot-production.crt
