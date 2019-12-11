39604fa4f6cb1d49e5425a8ae755abb0a34ddc8a
Henrik Johansson <henrik.johansson712@gmail.com>
2019-12-11 15:49:48 +0100

    Updated secret.sh

--- secret.sh
+++ secret.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-	openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout riot-dev.key -out riot-dev.crt -subj "/CN=scicat05.esss.lu.se" -days 3650
-	openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout riot-qa.key -out riot-qa.crt -subj "/CN=scicat06.esss.lu.se" -days 3650
-	openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout riot-production.key -out riot-production.crt -subj "/CN=scichat.esss.lu.se" -days 3650
+	openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout tls.key -out tls.crt -subj "/CN=scicat05.esss.lu.se" -days 3650
+	kubectl create secret -ndev tls riotservice --key tls.key --cert tls.crt
+	rm tls.*
 
+	openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout tls.key -out tls.crt -subj "/CN=scicat06.esss.lu.se" -days 3650
+	kubectl create secret -nqa tls riotservice --key tls.key --cert tls.crt
+	rm tls.*
 
-	kubectl create secret -ndev tls riotservice --key riot-dev.key --cert riot-dev.crt
-	kubectl create secret -nqa tls riotservice --key riot-qa.key --cert riot-qa.crt
-	kubectl create secret -nproduction tls riotservice --key riot-production.key --cert riot-production.crt
+	openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout tls.key -out tls.crt -subj "/CN=scichat.esss.lu.se" -days 3650
+	kubectl create secret -nproduction tls riotservice --key tls.key --cert tls.crt
+	rm tls.*
